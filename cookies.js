a86a1d2181e3d77b3c54996cfe9a9a45bf03facc
SnSDk <sergiosnsdk@gmail.com>
2023-03-15 01:08:45 -0300

    cookies-tutorial

---
+++ cookies.js
@@ -0,0 +1,22 @@
+
+
+//Almacenar info en cookie:
+document.cookie = "username=sergiecode; expires=Thu, 1 Sep 2022 00:00:00 UTC; path=/;"
+
+
+
+//Leer las cookies
+let infoDeCookie = document.cookie;
+
+
+
+//Modificar info en cookie:
+document.cookie = "username=@sergiecode; expires=Fri, 2 Sep 2022 00:00:00 UTC"
+
+
+
+//Borrar una cookie
+document.cookie = "username=; expires=Thu, 20 Mar 2020 00:00:00 UTC; path=/;"
+
+
+
